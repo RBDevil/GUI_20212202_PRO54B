a8b12940ee3ef787ade89d6ed631e4444ea74d00
ThtBnc <tothbence@stud.uni-obuda.hu>
2022-05-07 18:44:19 +0200

    gamelogic windowsize patch v2

--- GUI_20212202_PRO54B/Game/MainWindow.xaml.cs
+++ GUI_20212202_PRO54B/Game/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace Game
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            logic = new GameLogic(grid.RenderSize);
+            logic = new GameLogic();
             display.SetupLogic(logic);
 
             DispatcherTimer dt = new DispatcherTimer();
